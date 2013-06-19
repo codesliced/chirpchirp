148d2acfd126713dd2843c4adc209a4d1e049a03
codesliced <codesliced@gmail.com>
2013-06-19 07:47:14 -0500

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,31 @@
+get '/' do
+  erb :index
+end
+
+get '/sign_in' do
+  redirect request_token.authorize_url
+end
+
+get '/sign_out' do
+  session.clear
+  redirect '/'
+end
+
+get '/auth' do
+  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  session.delete(:request_token)
+
+  user = User.find_or_create_by_oauth_token(:username => @access_token.params[:screen_name],
+                           :oauth_token => @access_token.params[:oauth_token],
+                           :oauth_secret => @access_token.params[:oauth_token_secret])
+ session[:user_id] = user.id
+  erb :tweet
+  
+end
+
+post '/new/tweet' do
+  twitter_client.update(params[:body])
+  unless request.xhr?
+    erb :tweet
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+
+  validates :username,  presence: true
+  validates :oauth_token, presence: true,
+                          uniqueness: true
+  validates :oauth_secret, presence: true,
+                           uniqueness: true
+
+end
